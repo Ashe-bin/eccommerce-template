19f806bf25da9d6860e15d97ba748ff0bd5c4576
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2024-04-05 19:31:29 +0300

    eccommerce template

---
+++ product-page/script.js
@@ -0,0 +1,38 @@
+// focus the cursor on the email-address input
+const emailField = document.getElementById("email-address-input");
+emailField.focus({
+  preventScroll: true,
+});
+$(".like-btn").on("click", function () {
+  $(this).toggleClass("is-active");
+});
+
+$(".minus-btn").on("click", function (e) {
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest("div").find("input");
+  var value = parseInt($input.val());
+
+  if (value > 1) {
+    value = value - 1;
+  } else {
+    value = 0;
+  }
+
+  $input.val(value);
+});
+
+$(".plus-btn").on("click", function (e) {
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest("div").find("input");
+  var value = parseInt($input.val());
+
+  if (value < 100) {
+    value = value + 1;
+  } else {
+    value = 100;
+  }
+
+  $input.val(value);
+});
